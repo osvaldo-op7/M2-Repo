d80ba9419bbf53ee647b0d124029554c25158419
osvaldo-op7 <108706136+osvaldo-op7@users.noreply.github.com>
2022-10-19 07:05:22 -0500

    third homework

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -9,7 +9,14 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
-  
+  if(matchFunc(startEl)) resultSet.push(startEl);
+  if(startEl.children.length){
+    for(let el of startEl.children){
+      const resultado = traverseDomAndCollectElements(matchFunc, el);
+      resultSet = [...resultSet, ...resultado];
+    } 
+   }
+   return resultSet;
 };
 
 // Detecta y devuelve el tipo de selector
@@ -18,7 +25,12 @@ var traverseDomAndCollectElements = function(matchFunc, startEl) {
 
 var selectorTypeMatcher = function(selector) {
   // tu código aquí
-  
+  if(selector[0] === '#') return 'id';
+  if(selector[0] === '.') return 'class';
+  if(selector.split('.').length > 1) return 'tag.class';
+  return 'tag';
+
+
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -30,12 +42,27 @@ var matchFunctionMaker = function(selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
   if (selectorType === "id") { 
-   
+    matchFunction = (elemento) => {
+      if(`#${elemento.id}` === selector){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
   } else if (selectorType === "class") {
-    
+    matchFunction = (elemento) => {
+      return elemento.classList.contains(selector.replace(".",""));
+    }
   } else if (selectorType === "tag.class") {
+    matchFunction = (elemento) => {
+      const [tag, tagClass] = selector.split('.');
+      return (matchFunctionMaker(tag)(elemento) && matchFunctionMaker(`.${tagClass}`)(elemento));
+    }
     
   } else if (selectorType === "tag") {
+    matchFunction = (elemento) => elemento.tagName.toLowerCase() === selector;
+      
     
   }
   return matchFunction;
